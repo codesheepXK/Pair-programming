9bd8f49c06f9b15215ac8686202177d0d09ad284
codesheepXK <87645456+codesheepXK@users.noreply.github.com>
2022-10-11 00:45:18 +0800

    Add files via upload

---
+++ src/components/avatarBottom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="avatarBox">
+    <span>十亿少女的梦</span>
+    <img src="../assets/logo.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.avatarBox{
+    width: 180px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        width: 45px;
+        height: 45px;
+        border-radius: 50%;
+    }
+    span{
+        font-size: 18px;
+        font-weight: bold;
+        color: #fff;
+        margin-right: 20px;
+    }
+}
+</style>
---
+++ src/components/avatarTop.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="avatarBox">
+    <img src="../assets/logo.jpg">
+    <span>十亿少女的梦</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.avatarBox{
+    width: 180px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        width: 45px;
+        height: 45px;
+        border-radius: 50%;
+    }
+    span{
+        font-size: 18px;
+        font-weight: bold;
+        color: #fff;
+        margin-left: 20px;
+    }
+}
+</style>
