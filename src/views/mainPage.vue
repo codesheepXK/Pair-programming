d8ebf31b43fd879e987f4e30b42f576c1f980a78
codesheepXK <87645456+codesheepXK@users.noreply.github.com>
2022-10-11 00:35:18 +0800

    Create mainPage.vue

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,38 +1,74 @@
 <template>
   <div class="mainBox">
-    <div class="header">
-       <mt-button icon="back"  @click='$router.replace("/index")'></mt-button>
-       <span class="numStyle">当前数字：{{number}}</span>
-       <mt-button icon="more" ></mt-button>
+     <div class="btn song">
+      <i class="iconfont icon-yinlemusic215"></i>
+      <span>音乐</span>
     </div>
-    <diceBox :isTurn="!isTurn" pos="top" :BoardData="otherBoard"></diceBox>
-    <diceBox :isTurn="isTurn" pos="bottom" :BoardData="ownBoard"></diceBox>
-    <pageBottom></pageBottom>
+    <avatarTop class="avatarTop"></avatarTop>
+    <diceBox :isTurn="!isTurn" pos="top" :BoardData="otherBoard" class="diceBoxTop"></diceBox>
+    <div class="number">
+      <mt-button v-if="!isBegin" type="default" @click="gameStart">开始游戏</mt-button>
+      <img v-show="number==1" src="../assets/dice/dice1.png">
+      <img v-show="number==2" src="../assets/dice/dice2.png">
+      <img v-show="number==3" src="../assets/dice/dice3.png">
+      <img v-show="number==4" src="../assets/dice/dice4.png">
+      <img v-show="number==5" src="../assets/dice/dice5.png">
+      <img v-show="number==6" src="../assets/dice/dice6.png">
+    </div>
+    <diceBox :isTurn="isTurn" pos="bottom" :BoardData="ownBoard" class="diceBoxBottom"></diceBox>
+    <avatarBottom class="avatarBottom"></avatarBottom>
+    <div class="btn return" @click='$router.replace("/index")'>
+      <i class="iconfont icon-fanhui5"></i>
+      <span>返回</span>
+    </div>
+    <div class="btn back">
+      <i class="iconfont icon-chexiao-tianchong"></i>
+      <span>撤销</span>
+    </div>
+    <div class="btn out">
+      <i class="iconfont icon-shibai"></i>
+      <span>认输</span>
+    </div>
   </div>
 </template>
 
 <script>
+import avatarTop from "../components/avatarTop.vue"
+import avatarBottom from "../components/avatarBottom.vue"
 import diceBox from "../components/diceBox.vue"
-import pageBottom from "../components/pageBottom.vue"
-import {mapActions,mapGetters} from 'vuex';
+// import pageBottom from "../components/pageBottom.vue"
+import {mapMutations,mapGetters} from 'vuex';
 export default {
   data(){
     return{
+      isBegin:false,
+      needSong:true
     }
   },
   computed:{
-    ...mapGetters('dice',["number","isTurn","ownBoard","otherBoard"])
+    ...mapGetters('dice',["number","isTurn","ownBoard","otherBoard"]),
+    imgURL(){
+      return "../assets/dice/dice" + this.number +'.png'
+    }
   },
   components:{
+    avatarTop,
+    avatarBottom,
     diceBox,
-    pageBottom
   },
   methods:{
-     ...mapActions('dice',[
+     ...mapMutations('dice',[
      'updiceData',
      'updateTurn',
      'calSum',
-     'checkOver']),
+     'checkOver',
+     'getNum',
+     'updateReady']),
+     gameStart(){
+        this.isBegin=true;
+        this.getNum();
+        this.updateReady();
+     }
   }
 }
 </script>
@@ -42,39 +78,63 @@ export default {
   position: relative;
   width: 100%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
   background: linear-gradient(to right bottom,rgb(181, 225, 239),rgb(237, 237, 218));
-  // &::before{
-  //   content: '';
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 100%;
-  //   filter: blur(12px);
-  //   background: url("../assets/logo.jpg") no-repeat;
-  //   background-size: 100% 100%;
-  //    z-index: -1;
-  // };
-  .header{
-    width: 100%;
-    height: 40px;
-    position:relative;
+  .btn{
+    color: #fff;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    .numStyle{
-      text-align: center;
-      font-size: 18px;
-      font-weight: 600;
-      color: #fff;
+    span{
+      font-weight: bold;
     }
-    .mint-button {
-      background-color:inherit;
-      font-size: 12px;
-      color: #fff;
-      border: 0px;
-      box-shadow: none;
+    .iconfont{
+      font-size: 35px;
     }
+  }
+  .song{
+    position: absolute;
+    top :20px;
+    right: 20px;
+  }
+  .return{
+    position: absolute;
+    bottom  :5px;
+    left: 10px;
+  }
+  .back{
+    position: absolute;
+    bottom  :5px;
+    left: 60px;
+  }
+  .out{
+    position: absolute;
+    bottom  :5px;
+    left: 110px;
+  }
+  .number{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    margin: 10px 0px;
+    img{
+      width: 60px;
+      height: 60px;
+    }
+  }
+  .avatarTop{
+    align-self: flex-start;
+    margin: 10px 0px 10px 20px
+  }
+  .avatarBottom{
+    align-self: flex-end;
+    margin-right: 20px;
+    margin-top: 10px;
   }
 }
 
